4c2e74d5473d3b6fe888a621cd5b44353d03e24f
Ahmed <kayani479479@gmail.com>
2024-07-16 15:06:56 +0500

    Added trait Billable

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,9 +8,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 
 class User extends Authenticatable
 {
+    use Billable;
     /**
      * @var int|mixed
      */
